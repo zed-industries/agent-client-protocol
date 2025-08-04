c62132878b420d3ced036b50301a61c877ea6190
Michael Sloan <mgsloan@gmail.com>
2025-08-04 16:47:00 -0600

    Include constants in generated code

--- python/schema.py
+++ python/schema.py
@@ -4,6 +4,25 @@ from enum import Enum, IntEnum
 from typing import Union, Optional, Any, Generic, TypeVar, Callable, List, Tuple, Dict, TypedDict, NotRequired, Literal, Iterable, Iterator
 
 
+AGENT_METHODS = {
+    "authenticate": "authenticate",
+    "initialize": "initialize",
+    "session_cancel": "session/cancel",
+    "session_load": "session/load",
+    "session_new": "session/new",
+    "session_prompt": "session/prompt"
+}
+
+CLIENT_METHODS = {
+    "fs_read_text_file": "fs/read_text_file",
+    "fs_write_text_file": "fs/write_text_file",
+    "session_request_permission": "session/request_permission",
+    "session_update": "session/update"
+}
+
+PROTOCOL_VERSION = 1
+
+
 AgentClientProtocol = 'ClientRequest | ClientResponse | ClientNotification | AgentRequest | AgentResponse | AgentNotification'
 
 ClientRequest = 'WriteTextFileRequest | ReadTextFileRequest | RequestPermissionRequest'
